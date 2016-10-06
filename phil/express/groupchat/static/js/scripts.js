37364573838bbb7a5c2e6aa58da4e29a5a1a0924
Phil Boothe <pfb3cn@virginia.edu>
2016-10-06 18:24:25 -0400

    updated groupchat jquery and css

--- phil/express/groupchat/static/js/scripts.js
+++ phil/express/groupchat/static/js/scripts.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
     var user = prompt("Please enter your name:");
-    if (!user) {
+    if (!user || user.length>10) {
         $('#container').html(`<h1 id="login">Please enter a valid name.</h1>`);
     } else {
         var socket = io.connect();
+        var chatbox = $('#chat-wrapper');
 
         socket.emit('user_connect', { name: user });
+
         $('#send').click(function() {
             if ($('#message').val()) {
                 socket.emit("add_message", { message: $('#message').val() });
             }
-            $('#message').val('');
+            $('#message').val('').focus();
         });
 
         socket.on('send_full_chat', function(data) {
-            $('#chat-wrapper').html(data.chat);
+            chatbox.html(data.chat);
+            chatbox.scrollTop(chatbox.prop('scrollHeight'));
         });
         socket.on('send_chat_update', function(data) {
-            $('#chat-wrapper').append(data.content);
+            chatbox.append(data.content);
+            chatbox.scrollTop(chatbox.prop('scrollHeight'));
         });
     }
 });
