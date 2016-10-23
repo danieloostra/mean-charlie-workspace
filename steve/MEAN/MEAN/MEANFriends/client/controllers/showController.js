26cdc7604eea4b7ae2412f571812e8c50305e7e5
Stephen Hack <Stephen Hack>
2016-10-23 14:32:44 -0400

    Oct 22

---
+++ steve/MEAN/MEAN/MEANFriends/client/controllers/showController.js
@@ -0,0 +1,10 @@
+
+// app.controller('showController', ['$scope','friendsFactory', '$routeParams',    $scope.show = function(index) {
+//     console.log(index); 
+//     friendsFactory.show(index, function (returnedData) {
+//       $scope.friends = returnedData;
+//       console.log("New Controller Index" + index);
+//     });
+//    }
+// // show();
+// }]);
